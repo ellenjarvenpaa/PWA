640797661bb55782f884101f83bac936198c4901
ellen.jarvenpaa <ellen.jarvenpaa@metropolia.fi>
2023-09-10 16:10:14 +0300

    esim

---
+++ sw.js
@@ -0,0 +1,33 @@
+const files = [
+  "./index.html",
+  "./main.js",
+  "./main.css",
+  "./capybara-square-1.jpg.optimal.jpg",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      try {
+        const cache = await caches.open("v1");
+        return cache.addAll(files);
+      } catch (e) {
+        console.log(e.message);
+      }
+    })()
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log("ServiceWorker Fetch", event.request.url);
+  event.respondWith(
+    (async () => {
+      try {
+        const response = await caches.match(event.request);
+        return response || fetch(event.request);
+      } catch (e) {
+        console.log(e.message);
+      }
+    })()
+  );
+});
